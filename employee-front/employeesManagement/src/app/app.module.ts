8d5b6f3c96e0b5c50683de146275943b0d1ca9d4
Emmanuel Bassi <emmanuel.mktg@gmail.com>
2022-08-28 17:39:55 -0300

    first commit

--- employee-front/employeesManagement/src/app/app.module.ts
+++ employee-front/employeesManagement/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { EmployeesListComponent } from './employees-list/employees-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    EmployeesListComponent
   ],
   imports: [
     BrowserModule,
---
+++ employee-front/employeesManagement/src/app/employees-list/employees-list.component.ts
@@ -0,0 +1,18 @@
+import { employeeInterface } from './../interfaces/employee.interface';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employees-list',
+  templateUrl: './employees-list.component.html',
+  styleUrls: ['./employees-list.component.scss']
+})
+export class EmployeesListComponent implements OnInit {
+
+  employees:employeeInterface[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
